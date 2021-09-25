76354710f3483eec1c84ea12b8c781e93c04748b
Hubby <hubs@Dianas-MacBook-Pro.local>
2021-09-26 10:24:09 +1300

    upgrade to multi-crypto support

--- rsibot/bot.py
+++ rsibot/bot.py
@@ -25,23 +25,16 @@ class CryptoAsset():
     Data Structure for a crypto asset.
     Will store info regarding a particular crypto currency and its progress,
     with the ability to save the state to disk for later recovery.
-    """ 
+    """
 
-    def __init__(self, trade_symbol="ETHUSD"):
+    def __init__(self, trade_symbol="ETHUSD",simulation=False):
         self.trade_symbol = trade_symbol
         self.in_position = False # is money committed to asset?
         self.candle_data = {}
-        self.simulation = True
+        self.simulation = simulation
         self.max_funds = 100
         self.trade_quantity = 0.05
 
-        #add other data here
-
-
-        # todo: store per-symbol tracker data in a text doc
-        if self.trade_symbol=="ETHUSD":
-            # todo: add interval argument
-            self.binance_socket = "wss://stream.binance.com:9443/ws/ethusdt@kline_1m"
 
 
     def save(self):
@@ -63,7 +56,7 @@ class CryptoBot():
     """
 
 
-    def __init__(self, simulation=True):
+    def __init__(self):
 
         # RSI Attributes (todo: initialize in a factory class)
         self.RSI_PERIOD = 14
@@ -72,12 +65,26 @@ class CryptoBot():
 
         self.assets = []
 
+    def add_asset(self, asset):
+        """ Add the provided asset to a list of currently processed assets.
+        Args:
+            asset (CryptoAsset): An instance of the CryptoAsset class, containing
+                information about the asset.
+
+        """
+        self.assets.append(asset)
+        # do any other initialization here, such as loading asset history
+    def get_symbols(self):
+        return list(set([asset.trade_symbol for asset in self.assets]))
 
     def process_data(self, candle_data):
         """ For a set of candle data, process the indicators
         and then determine a course of action: Buy, Sell, Pass.
         """
-        for asset in assets:
+        for asset in self.assets:
+            print (candle_data)
+            print (asset.trade_symbol)
+            asset.candle_data = candle_data[asset.trade_symbol]
             rsi_results = self.rsi_indicator(asset.candle_data)
             #todo: turn this into a factory of indicators
 
@@ -142,20 +149,9 @@ class CryptoBot():
 
             # move this section to process_indicators
             if last_rsi > self.RSI_OVERBOUGHT:
-                if self.in_position:
-                    print("Overbought! Sell! Sell! Sell!")
-                    # put binance sell logic here
-                    return SIDE_SELL
-                else:
-                    print("It is overbought, but we don't own any. Nothing to do.")
-            
+                return SIDE_SELL
             if last_rsi < self.RSI_OVERSOLD:
-                if in_position:
-
-                    print("It is oversold, but you already own it, nothing to do.")
-                else:
-                    print("Oversold! Buy! Buy! Buy!")
-                    return SIDE_BUY
+                return SIDE_BUY
         return None
 
 
@@ -171,13 +167,19 @@ class TickerListener():
     indicators as needed
     """
 
-    def __init__(self, callback=None, assets=[], simulation=False):
+    def __init__(self, callback=None, symbols=[], simulation=False):
         
         self.callback = callback
+        self.symbols = symbols
 
-        self.candle_data = {
-            'closes':[]
-        }
+        self.simulation = simulation
+        self.candle_data = {}
+
+        # initialize candle data dict
+        for symbol in symbols:
+            self.candle_data[symbol]={
+                'closes':[]
+                }
 
     def on_open(self, ws):
         print('opened connection') 
@@ -186,65 +188,85 @@ class TickerListener():
         print('closed connection')
 
     def on_message(self, ws, message):   
+        """TODO: expand for multiple symbols"""
         json_message = json.loads(message)
         #pprint.pprint(json_message)
         self.store_closes(json_message)
-        self.callback(self.candle_data)
+        self.broadcast_candle_data()
         # store other data and trigger events as needed
         
-    def send_message(self):
-        self.trigger(candle_data)
+    def broadcast_candle_data(self):
+        self.callback(self.candle_data)
 
     def store_closes(self, json_message):
-        candle = json_message['k']
-        is_candle_closed = candle['x']
-        close = candle['c']
-        if is_candle_closed:
-            print("candle closed at {}".format(close))
-            self.candle_data['closes'].append(float(close))
-            if self.trigger_close:
-                self.trigger_close(self.candle_data)
-                print('triggered')
+        """TODO: expand for multiple symbols"""
+        for symbol in self.symbols:
+            candle = json_message['k']
+            is_candle_closed = candle['x']
+            close = candle['c']
+            if is_candle_closed:
+                print("candle closed at {}".format(close))
+                self.candle_data[symbol]['closes'].append(float(close))
+                if self.trigger_close:
+                    self.trigger_close(self.candle_data)
 
 
     def retrieve_binance_data(self):
         #todo: check api and configure for tracking multiple assets
+        if self.callback:
+            if len(self.symbols)>1:
+                raise RuntimeError('Only a single symbol is supported at the moment')
+            for symbol in self.symbols:
+                ws = websocket.WebSocketApp(
+                    self.get_binance_socket(symbol), 
+                    on_open=self.on_open, 
+                    on_close=self.on_close,
+                    on_message=self.on_message
+                )
 
-        s = websocket.WebSocketApp(
-            asset.binance_socket, 
-            on_open=self.on_open, 
-            on_close=self.on_close,
-            on_message=self.on_message
-        )
-        ws.run_forever()
+            # RIGHT NOW THIS ONLY SUPPORTS ONE TICKER
+            ws.run_forever()
+
+
+    def retrieve_simulated_data(self):
+        for symbol in self.symbols:
+
+            # make an arbitrary array of close info
+            fake_close_data = [float(i) for i in range(1,20)]
+            self.candle_data[symbol]={
+                'closes':fake_close_data,
+                }
+            self.broadcast_candle_data()
 
     def run(self):
         if self.simulation:
             self.retrieve_simulated_data()
 
         else:
-            self.retrieve_binance_data():
+            self.retrieve_binance_data()
+
+
+    @staticmethod
+    def get_binance_socket(symbol, rate="1m"):
+        """ Temp hack to get socket"""
+        if symbol=="ETHUSD":
+            binance_socket = "wss://stream.binance.com:9443/ws/ethusdt@kline_{}".format(rate)
+
+        return binance_socket
 
 
 
-    def retrieve_simulated_data(self):
-
-        for asset in self.assets:
-
-            # make an arbitrary array of close info
-            fake_close_data = [float(i) for i in range(1,20)]
-            self.candle_data[asset.trade_symbol]={
-                'closes':fake_close_data,
-                }
-            }
-
-
-
+# Initialize a crypto asset
 eth =  CryptoAsset(trade_symbol="ETHUSD", simulation=True)   
-bot = CryptoBot(asset)
+
+#Add it to our bot
+bot = CryptoBot()
 bot.add_asset(eth)
 
+# get a list of all the symbols our bot cares about, for tracking
+symbols = bot.get_symbols()
 
-# run the listener for live data:     
-listener = TickerListener(callback=bot.process_data, simulation=True, assets=CryptoBot.assets)
+# run the listener for live data
+# a callback will trigger our bot to evaluate  
+listener = TickerListener(callback=bot.process_data, simulation=True, symbols=symbols)
 listener.run()
